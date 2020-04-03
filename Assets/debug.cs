650a121c0d03ecb47da837e2740ed96165b63872
poomwarning <thanaphum.14@mail.kmutt.ac.th>
2020-04-03 22:00:41 +0700

    เสร็จแล้ว

---
+++ Assets/debug.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debug : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) 
+    {
+      
+        Debug.Log("WIN THE FUCK");
+    
+   }
+}
---
+++ Assets/gate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gate : MonoBehaviour
+{
+   [SerializeField]
+   GameObject door ;
+
+
+    void OnTriggerEnter(Collider other) 
+    {
+      
+           door.transform.position += new Vector3(0,4,0);
+    
+   }
+}
